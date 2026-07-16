caf6ecf331e59d1a4751eb79e8aa0599ea64ef08
Rahul Dookwah <rahuldookwah@gmail.com>
2026-07-15 23:00:42 -0400

    Update Playlist model for playlist scene

--- src/main/java/model/Playlist.java
+++ src/main/java/model/Playlist.java
@@ -3,19 +3,52 @@ package model;
 import java.util.ArrayList;
 
 public class Playlist implements Scorable {
+    private String name;
     private ArrayList<Song> songs; //list of songs in playlist
     private boolean isAnAlbum; //determines if this collection of songs is an album
 
     /**
-     * constructor
-     *
+     * Constructor
+     * @param name      Name of playlist - RD
      * @param songs     list of songs in playlist
      * @param isAnAlbum is this an album?
      */
-    public Playlist(ArrayList<Song> songs, boolean isAnAlbum) {
+    public Playlist(String name, ArrayList<Song> songs, boolean isAnAlbum) {
+        this.name = name;
         this.songs = songs;
         this.isAnAlbum = isAnAlbum;
     }
+
+    /**
+     * getName returns playlist name - RD
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    // Sets name of playlist, used to rename playlist - RD
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * getSongs returns playlist songs within playlist - RD
+     * @return songs
+     */
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    /**
+     * getSongsCount returns playlist size - RD
+     * @return songs.size()
+     */
+    public int getSongsCount() {
+        return songs.size();
+    }
+
+
 
     /**
      * genreScore
